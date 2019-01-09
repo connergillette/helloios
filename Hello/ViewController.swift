c27de1547b59e73641a4d3bb0b7e77a7dec9b6d9
Conner Gillette <connerwgillette@gmail.com>
2019-01-08 22:25:52 -0800

    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Student User on 1/8/19.
+//  Copyright © 2019 Conner Gillette. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
